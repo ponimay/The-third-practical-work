84507a821fed59772927ca4a71916304cff0cfb5
ponimay <kirpichnikov.nik2019@yandex.ru>
2024-02-21 23:55:03 +0300

    The MVVM task

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,7 +8,14 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myapplication.databinding.ActivityMainBinding
+import kotlin.properties.ReadOnlyProperty
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textView: TextView
@@ -18,7 +25,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel:PostViewModel by viewModels()
+        viewModel.data.observe(this){post->
+            with(binding){
+                textView2.text = post.author
+                textView3.text = post.published
+                textView4.text = post.content
+
+
+            }
+
+        }
+
+
+
         textView = findViewById(R.id.textView6)
         textView6 = findViewById(R.id.textView)
         var imageButton2: ImageButton = (findViewById(R.id.imageButton3))
@@ -28,7 +52,7 @@ class MainActivity : AppCompatActivity() {
         var root: ConstraintLayout = (findViewById(R.id.root))
 
         root.setOnClickListener {
-            Toast.makeText(this, "Вы нажали на ConstraintLayout", Toast.LENGTH_SHORT).show()
+           Toast.makeText(this, "Вы нажали на ConstraintLayout", Toast.LENGTH_SHORT).show()
         }
 
         imageButton.setOnClickListener {
@@ -41,7 +65,7 @@ class MainActivity : AppCompatActivity() {
         }
         imageButton2.setOnClickListener {
             likes--
-            Toast.makeText(this, "Вы дизлайкнули новый пост", Toast.LENGTH_SHORT).show()
+           Toast.makeText(this, "Вы дизлайкнули новый пост", Toast.LENGTH_SHORT).show()
 
             updatelikes()
 
@@ -51,7 +75,7 @@ class MainActivity : AppCompatActivity() {
 
         imageButton3.setOnClickListener {
             share += 10
-            Toast.makeText(this, "Вы поделились новым постом", Toast.LENGTH_SHORT).show()
+           Toast.makeText(this, "Вы поделились новым постом", Toast.LENGTH_SHORT).show()
 
             updateshare()
         }
@@ -65,15 +89,61 @@ class MainActivity : AppCompatActivity() {
     private fun updateshare() {
         textView6.text = if (share > 999) {
             String.format("%.1fK", share / 1000.0)
-        } else {
+        } else if(share > 999999) {
+            String.format("%.1fM", share / 1000_000.0)
+        }
+        else{
             share.toString()
+
+
         }
     }
     private fun updatelikes() {
         textView.text = if (likes > 999) {
             String.format("%.1fK", likes / 1000.0)
-        } else {
+        } else if(likes > 999999) {
+            String.format("%.1fM", likes / 1000_000.0)
+        }
+        else{
             likes.toString()
+
+
         }
     }
 }
+
+data class Post(
+    val id:Long,
+    val author:String,
+    val content:String,
+    val published:String,
+    val likedByMe:Boolean,
+)
+interface PostRepository{
+    fun get(): LiveData<Post>
+    fun like()
+}
+class PostRepositoryInMemoryImpl:PostRepository{
+    private var post = Post(
+        id = 1,
+        author = "ГБПОУ ВО 'БТПИТ'",
+        content = "Студенты 1-4 курсов специальности: \"Дошкольное образование\" Борисоглебского колледжа промышленных и информационных технологий, совместно с преподавателями Ларисой Владимировной Гребенниковой и Маргаритой Михайловной Луговиной, 13 февраля, на базе Борисоглебского филиала ВИРО им. Н.Ф. Бунакова, в рамках мероприятий Наставник клуба молодых педагогов, принял участие в семинаре на тему: \"Методы и методички формирования исследовательского поведения ребенка как условие его саморазвития\".\"В ходе этого образовательного интенсива Ольга Николаевна, региональный методист, педагог-психолог МБДОУ \"Новохоперский центр развития ребенка \"Пристань детства\", поделилась своим опытом работы. Подробнее читайте на нашем сайте -> https://btpit36.ru",
+        published = "20 февраля в 09:00",
+        likedByMe = false
+    )
+    private val data = MutableLiveData(post)
+
+
+    override fun get(): LiveData<Post> = data
+
+    override fun like() {
+        post = post.copy(likedByMe = !post.likedByMe)
+        data.value = post
+    }
+}
+class PostViewModel: ViewModel() {
+    private val repository:PostRepository = PostRepositoryInMemoryImpl()
+    val data = repository.get()
+    fun like() = repository.like()
+}
+
