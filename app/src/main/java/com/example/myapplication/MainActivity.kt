7b3f034d8ca21da3b45a2a7b0ae6cac06b5fc048
ponimay <kirpichnikov.nik2019@yandex.ru>
2024-02-22 18:02:36 +0300

    The MVVM task 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -109,6 +109,6 @@ class PostViewModel: ViewModel() {
     private val repository:PostRepository = PostRepositoryInMemoryImpl()
     val data = repository.get()
     fun like() = repository.like()
-    fun share() = repository.share() 
+    fun share() = repository.share()
 }
 
